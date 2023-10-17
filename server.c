a0d5222a564ed664b13685236725da83c20f2b5b
Sebo1411 <112760823+Sebo1411@users.noreply.github.com>
2023-10-17 16:09:07 +0200

    Nova  nedovrsena verzija

--- server.c
+++ server.c
@@ -1,15 +1,19 @@
 #ifndef _WIN32_WINNT
 #define _WIN32_WINNT 0x0600
 #endif
+#pragma comment(lib, "ws2_32.lib")
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#pragma comment(lib, "ws2_32.lib")
 
 #include <stdio.h>
+#include <stdlib.h>
 
 //primjer 1 - jedno spajanje i preinaka poruke
 
 int main(){
+
+    SOCKADDR_STORAGE addressFamily;
+    addressFamily.ss_family=(ADDRESS_FAMILY) AF_INET;
 
     /*
      *  IPv4
@@ -17,10 +21,16 @@ int main(){
      *  TCP
      *
      */
-    SOCKET socFileDesc=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
-    if (socFileDesc==INVALID_SOCKET){
+    SOCKET sock=socket(addressFamily.ss_family,SOCK_STREAM,IPPROTO_TCP);
+    if (sock==INVALID_SOCKET){
         puts(WSAGetLastError());//funkcija za hendlanje
         return -1;
     }
+    
+    if (bind(sock,(SOCKADDR_STORAGE *)&addressFamily,sizeof(addressFamily))==SOCKET_ERROR){
+        puts(WSAGetLastError());//funkcija za hendlanje
+        return -1;
+    }
+
 
 }
